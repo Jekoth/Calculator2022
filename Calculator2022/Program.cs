b0d327cbf95897b2d35df2a6de68593cb1264ee7
Rizvan Gataev <Rizvan8103@gmail.com>
2022-04-05 10:00:29 +0300

    Initial commit

---
+++ Calculator2022/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First number");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number");
+            int second = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
